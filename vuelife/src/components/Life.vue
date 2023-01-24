f46d1a78c8da66152d120d1cf9b356b3bed6a5d3
Glória Brito <gloriafbrito@gmail.com>
2023-01-24 00:14:58 -0300

    insere tabelas

---
+++ vuelife/src/components/Life.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <h1>Hello Life {{ msg }}</h1>
+    <table>
+        <tr>
+            <td>1</td>
+            <td>2</td>
+        </tr>
+        <tr>
+            <td>3</td>
+            <td>4</td>
+        </tr>
+    </table>
+    </div>
+</template>
+
+<script>
+export default{
+    data() {
+        return {
+            msg: '{{vai aparecer uma mensagem aqui}}'
+        }
+    }
+}
+</script>
