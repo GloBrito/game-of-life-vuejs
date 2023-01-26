8e42df75a4b049c8eacc89e302f533b955950641
Glória Brito <gloriafbrito@gmail.com>
2023-01-25 22:23:32 -0300

    modifica style

--- vuelife/src/components/Life.vue
+++ vuelife/src/components/Life.vue
@@ -3,14 +3,16 @@
         <table>
             <tr v-for="(line, i) in grid" :key="i">
                 <td v-for="(celula, y) in line" :key="y" :class="{celulaViva: celula==1}"></td>
-                <td></td>
             </tr>
         </table>
     </div>    
 </template>
+
 <script>
 
+
 function contaVizinhosVivos(i,y,grid){
+    const [x, z] = [grid.length, grid[0].length]
     const deltaCelulaContigua = [
         [-1,-1], [-1,0], [-1,-1],
         [0,-1], [0,1],
@@ -18,11 +20,12 @@ function contaVizinhosVivos(i,y,grid){
     ]
     let coordVizinhos = []
     for (let k = 0; k < deltaCelulaContigua.length; k++){
-        coordVizinhos.push([i + deltaCelulaContigua[k][0], y + deltaCelulaContigua[k][0]])
+        coordVizinhos.push([i + deltaCelulaContigua[k][0], y + deltaCelulaContigua[k][1]])
     }
-    console.log(i, y, grid)
-    return 0
+    let coordVizinhosVivos = coordVizinhos.filter((e) => e[0] >= 0 && e[0] < x && e[1] < z && grid[e[0]][e[1]] == 1)
+    return coordVizinhosVivos.length
 }
+
 function _nextState(grid){
     const [x, z] = [grid.length, grid[0].length]
     let newGrid = new Array(x)
@@ -41,7 +44,8 @@ function _nextState(grid){
             } else {
                 if (vizinhosVivos == 3){
                     newGrid[i][y] = 1
-                } else {
+                } 
+                else {
                     newGrid[i][y] = 0
                 }
             }
@@ -50,51 +54,49 @@ function _nextState(grid){
     return newGrid
 }
 
-export default ({
+export default {
     name: 'Life',
     data() {
-    return {
-        grid: [ //matriz inicial
-            [0,0,0,0,0,0,0,0,0],
-            [0,0,0,0,0,0,0,0,0],
-            [0,1,1,1,0,1,1,1,0],
-            [1,0,0,0,1,0,1,0,1],
-            [0,1,0,0,0,0,0,1,0],
-            [0,0,1,0,0,0,1,0,0],
-            [0,0,0,1,0,1,0,0,0],
-            [0,0,0,0,1,0,0,0,0],
-            [0,0,0,0,0,0,0,0,0],
-            [0,0,0,0,0,0,0,0,0],
-        ]
-    }
-},
-    methods: {
-    nextStep() {
-        this.grid = _nextState(this.grid)
-    },    
-},
-methods: {
-    nextStep(){
-            this.grid = _nextState(this.grid)
+        return {
+            grid: [ //matriz inicial
+                [0,0,0,0,0,0,0,0,0,0],
+                [0,0,0,0,0,1,1,0,0,0],
+                [0,0,0,0,1,0,1,0,0,0],
+                [0,0,0,0,0,0,1,0,0,0],
+                [0,0,0,0,0,0,0,0,0,0],
+                [0,0,1,0,0,0,0,0,0,0],
+                [0,0,1,0,0,0,0,1,1,1],
+                [0,1,1,0,0,0,1,1,0,0],
+                [0,1,0,0,0,0,1,0,0,0],
+                [0,0,0,0,0,0,0,0,0,0],
+            ]
         }
     },
+    methods: {
+        nextStep(){
+            this.grid = _nextState(this.grid)
+    },
+    },
     mounted(){
-        setInterval (() => {
+        setInterval(() => {
             this.nextStep()
-        }, 300)
+        }, 1000)
     }
-})
+  }
 </script>
 
 <style>
-table, th, td{
+table, th, td {
+    padding: .7em;
+    margin: 300px;
     border: 1px solid;
 }
 td {
     width: 30px;
     height: 30px;
 }
-.vivo{
-    background-color: yellow;
+
+.celulaViva {
+    background-color: #9c27b0;
 }
 </style>
